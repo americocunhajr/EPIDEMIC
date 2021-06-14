8c5b222208f12a7ae59f8abe62a30bc513448344
BrunaPavlack <65251862+BrunaPavlack@users.noreply.github.com>
2021-06-14 17:54:36 -0300

    Update check_error_SEIAHRD.m

--- modeling/SEIAHRD/check_error_SEIAHRD.m
+++ modeling/SEIAHRD/check_error_SEIAHRD.m
@@ -44,17 +44,17 @@ disp(' -----------------------------------------------')
 disp(' ++++++++++++ rhs_SEIAHRD checking +++++++++++++')
 % -----------------------------------------------------------
 
-% Case 1: length(param) < 9 
+% Case 1: length(param) < 8
 % -----------------------------------------------------------  
 t = 1;                                      % time
 y = [999 0 1 0 0 0 0 1];                    % state vector
-param = [1000 0.5 0.5 0.1 0.1 0.5 0.2 0.3]; % param. vector
+param = [1000 0.5 0.1 0.1 0.5 0.2 0.3]; % param. vector
 
 try  
     rhs_SEIAHRD(t,y,param)
 catch ME
     disp(' ')
-    disp('Case 1: length(param) < 9')
+    disp('Case 1: length(param) < 8')
     disp(['t: ',num2str(t)])
     disp(['y: ',num2str(y)])
     disp(['param: ',num2str(param)])
@@ -66,15 +66,15 @@ catch ME
 end
 % ----------------------------------------------------------- 
 
-% Case 2: length(param) > 9
+% Case 2: length(param) > 8
 % -----------------------------------------------------------  
-param = [1000 0.5 0.5 0.1 0.1 0.5 0.2 0.3 0.2 0.2]; % parameters vector
+param = [1000 0.5 0.1 0.1 0.5 0.2 0.3 0.2 0.2]; % parameters vector
 
 try  
     rhs_SEIAHRD(t,y,param)
 catch ME
     disp(' ')
-    disp('Case 2: length(param) > 9')
+    disp('Case 2: length(param) > 8')
     disp(['t: ',num2str(t)])
     disp(['y: ',num2str(y)])
     disp(['param: ',num2str(param)])
@@ -88,7 +88,7 @@ end
 
 % Case 3: mod(param(1),1) ~= 0
 % -----------------------------------------------------------  
-param = [1000.25 0.5 0.5 0.5 0.1 0.3 0.2 0.2 0.2]; % parameters vector
+param = [1000.25 0.5 0.5 0.1 0.3 0.2 0.2 0.2]; % parameters vector
 
 try  
     rhs_SEIAHRD(t,y,param)
@@ -108,7 +108,7 @@ end
 
 % Case 4: param(1) < 0
 % -----------------------------------------------------------  
-param = [-1000 0.5 0.5 0.5 0.1 0.3 0.2 0.2 0.2]; % parameters vector
+param = [-1000 0.5 0.5 0.1 0.3 0.2 0.2 0.2]; % parameters vector
 
 try  
     rhs_SEIAHRD(t,y,param)
@@ -128,7 +128,7 @@ end
 
 % Case 5: param(2) < 0
 % -----------------------------------------------------------  
-param = [1000 -0.5 0.5 0.5 0.1 0.3 0.2 0.2 0.2]; % parameters vector
+param = [1000 -0.5 0.5 0.1 0.3 0.2 0.2 0.2]; % parameters vector
 
 try  
     rhs_SEIAHRD(t,y,param)
@@ -148,7 +148,7 @@ end
 
 % Case 6: param(3) < 0
 % -----------------------------------------------------------  
-param = [1000 0.5 -0.5 0.5 0.1 0.3 0.2 0.2 0.2]; % parameters vector
+param = [1000 0.5 -0.5 0.1 0.3 0.2 0.2 0.2]; % parameters vector
 
 try  
     rhs_SEIAHRD(t,y,param)
@@ -168,7 +168,7 @@ end
 
 % Case 7: param(4) < 0
 % -----------------------------------------------------------  
-param = [1000 0.5 0.5 -0.5 0.1 0.3 0.2 0.2 0.2]; % parameters vector
+param = [1000 0.5 0.5 -0.1 0.3 0.2 0.2 0.2]; % parameters vector
 
 try  
     rhs_SEIAHRD(t,y,param)
@@ -188,7 +188,7 @@ end
 
 % Case 8: param(5) < 0
 % -----------------------------------------------------------  
-param = [1000 0.5 0.5 0.5 -0.1 0.3 0.2 0.2 0.2]; % parameters vector
+param = [1000 0.5 0.5 0.1 -0.3 0.2 0.2 0.2]; % parameters vector
 
 try  
     rhs_SEIAHRD(t,y,param)
@@ -208,7 +208,7 @@ end
 
 % Case 9: param(6) < 0
 % -----------------------------------------------------------  
-param = [1000 0.5 0.5 0.5 0.1 -0.3 0.2 0.2 0.2]; % parameters vector
+param = [1000 0.5 0.5 0.1 0.3 -0.2 0.2 0.2]; % parameters vector
 
 try  
     rhs_SEIAHRD(t,y,param)
@@ -228,7 +228,7 @@ end
 
 % Case 10: param(7) < 0
 % -----------------------------------------------------------  
-param = [1000 0.5 0.5 0.5 0.1 0.3 -0.2 0.2 0.2]; % parameters vector
+param = [1000 0.5 0.5 0.1 0.3 0.2 -0.2 0.2]; % parameters vector
 
 try  
     rhs_SEIAHRD(t,y,param)
@@ -248,7 +248,7 @@ end
 
 % Case 11: param(8) < 0
 % -----------------------------------------------------------  
-param = [1000 0.5 0.5 0.5 0.1 0.3 0.2 -0.2 0.2]; % parameters vector
+param = [1000 0.5 0.5 0.1 0.3 0.2 0.2 -0.2]; % parameters vector
 
 try  
     rhs_SEIAHRD(t,y,param)
@@ -266,15 +266,16 @@ catch ME
 end
 % -----------------------------------------------------------
 
-% Case 12: param(9) < 0
+
+% Case 13: param(4) > 1
 % -----------------------------------------------------------  
-param = [1000 0.5 0.5 0.5 0.1 0.3 0.2 0.2 -0.2]; % parameters vector
+param = [1000 0.5 0.5 1.1 0.3 0.2 0.2 0.1]; % parameters vector
 
 try  
     rhs_SEIAHRD(t,y,param)
 catch ME
     disp(' ')
-    disp('Case 12: param(9) < 0')
+    disp('Case 13: param(4) > 1')
     disp(['t: ',num2str(t)])
     disp(['y: ',num2str(y)])
     disp(['param: ',num2str(param)])
@@ -286,55 +287,15 @@ catch ME
 end
 % -----------------------------------------------------------
 
-% Case 13: param(3) > 1
+% Case 14: param(8) > 1
 % -----------------------------------------------------------  
-param = [1000 0.5 1.1 0.5 0.1 0.3 0.2 0.2 0.2]; % parameters vector
+param = [1000 0.5 0.5 0.5 0.3 0.2 0.2 1.1]; % parameters vector
 
 try  
     rhs_SEIAHRD(t,y,param)
 catch ME
     disp(' ')
-    disp('Case 13: param(3) > 1')
-    disp(['t: ',num2str(t)])
-    disp(['y: ',num2str(y)])
-    disp(['param: ',num2str(param)])
-    disp(' ')
-    disp(ME.message)
-    disp(' ')
-    disp('Error log checked!')
-    disp(' ')
-end
-% -----------------------------------------------------------
-
-% Case 14: param(5) > 1
-% -----------------------------------------------------------  
-param = [1000 0.5 0.5 0.5 1.1 0.3 0.2 0.2 0.1]; % parameters vector
-
-try  
-    rhs_SEIAHRD(t,y,param)
-catch ME
-    disp(' ')
-    disp('Case 14: param(5) > 1')
-    disp(['t: ',num2str(t)])
-    disp(['y: ',num2str(y)])
-    disp(['param: ',num2str(param)])
-    disp(' ')
-    disp(ME.message)
-    disp(' ')
-    disp('Error log checked!')
-    disp(' ')
-end
-% -----------------------------------------------------------
-
-% Case 15: param(9) > 1
-% -----------------------------------------------------------  
-param = [1000 0.5 0.5 0.5 0.5 0.3 0.2 0.2 1.1]; % parameters vector
-
-try  
-    rhs_SEIAHRD(t,y,param)
-catch ME
-    disp(' ')
-    disp('Case 15: param(9) > 1')
+    disp('Case 14: param(8) > 1')
     disp(['t: ',num2str(t)])
     disp(['y: ',num2str(y)])
     disp(['param: ',num2str(param)])
@@ -347,14 +308,14 @@ end
 % -----------------------------------------------------------
 
 
-% Case 16: valid input
+% Case 15: valid input
 % -----------------------------------------------------------  
-param = [1000 0.5 0.5 0.5 0.1 0.3 0.2 0.2 0.2]; % parameters vector
+param = [1000 0.5 0.5 0.1 0.3 0.2 0.2 0.2]; % parameters vector
  
 dy = rhs_SEIAHRD(t,y,param);
 
 disp(' ')
-disp('Case 16: valid input')
+disp('Case 15: valid input')
 disp(['t :',num2str(t)])
 disp(['y: ',num2str(y)])
 disp(['param: ',num2str(param)])
