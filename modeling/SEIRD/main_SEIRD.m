758e895d8451d8c78e40f8d31d5dac827abf5ddc
Michel Tosin <54448072+TosinMA@users.noreply.github.com>
2022-01-20 15:02:06 -0300

    Update main_SEIRD.m

--- modeling/SEIRD/main_SEIRD.m
+++ modeling/SEIRD/main_SEIRD.m
@@ -209,6 +209,8 @@ hold off
     xlim([t0 t1]);
     ylim([0 N0]);
 
+    saveas(figure(1),'fig_modeling_SEIRD_example1_compart.png')
+
 
 % plot NewCases (per day) of SEIRD model
 figure(2)
@@ -225,4 +227,6 @@ fig2 = scatter([1:length(NewCases)]+1,NewCases);
 
     % axis limits
     xlim([t0 length(NewCases)]+1);
+
+    saveas(figure(2),'fig_modeling_SEIRD_example1_newcases.png')
 % -----------------------------------------------------------
